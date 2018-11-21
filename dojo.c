348a14e726f51eada306711229bbc9a301e32299
tfabiha <tfabiha@stuy.edu>
2018-11-21 10:46:53 -0500

    adding

--- dojo.c
+++ dojo.c
@@ -3,8 +3,17 @@
 #include <unistd.h>
 
 int main(int argc, char **argv){
-  while(1){
-    
+  while(1)
+    {
+      char str[60];
+      printf("this is working\n");
 
-  }
+      if (fgets(str, 60, stdin) != NULL)
+	{
+	  printf("I got this from you: %s\n", str);
+	  return 0;
+	}
+
+    }
+
 }
