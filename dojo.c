56f1a3df64c8a73585dbf413b06277b2289cfef3
ivzhang1 <izhang1@stuy.edu>
2018-11-24 23:29:58 -0500

    DW is going to love these colors

--- dojo.c
+++ dojo.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv){
     char cmds[100];
     char cwd[1024];
     getcwd(cwd, sizeof(cwd));
-    printf("%s Dojo:$ ",cwd);
+    printf("\033[32m%s \033[0m \033[31mDojo:\033[0m \033[35m$\033[0m \033[34m",cwd);
     if (fgets(cmds, 60, stdin) == NULL)
     {
       printf("Error has been found. Exiting.\n");
